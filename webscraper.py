a83c550c0de515dd603efac9a164a0dddcf9c2d3
Geogeo357 <geogeo357@gmail.com>
2020-02-20 16:46:42 -0600

    directions now work too
    web scraper v1 is finished :)

--- webscraper.py
+++ webscraper.py
@@ -5,15 +5,18 @@ def is_ingredient(tag):
     return tag.name == "span" and tag.has_attr('itemprop') and tag['itemprop'] == "recipeIngredient"
 
 def is_direction(tag):
-    return tag.name == "span" and tag.has_attr('class') and tag['class'] == "recipe-directions__list--item"
+    return tag.name == "span" and tag.has_attr('class') and "recipe-directions__list--item" in tag['class']
 
 def parse_recipe(url):
     request_url = urllib.request.urlopen(url)
     web_html = str(request_url.read())
     soup = BeautifulSoup(web_html, 'html.parser')
     ingredients = [span.contents[0] for span in soup.find_all(is_ingredient)]
-    directions = [span.contents[0] for span in soup.find_all(is_direction)]
+    soup = BeautifulSoup(web_html, 'html.parser')
+    directions = [span.contents[0].strip()[:-2] if len(span.contents) > 0 else '' for span in soup.find_all(is_direction)]
+    while '' in directions:
+        directions.remove('')
     return {'ingredients': ingredients, 'directions': directions}
 
 if __name__ == '__main__':
-    print(parse_recipe("https://www.allrecipes.com/recipe/219634/chef-johns-french-fries/"))
+    print(parse_recipe("https://www.allrecipes.com/recipe/10813/best-chocolate-chip-cookies/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=219634%20referringContentType%3Drecipe"))
